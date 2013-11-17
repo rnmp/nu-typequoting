80b624a4f649fd80762c926aca69a5e7cb4ea400
Rolando Murillo <rolando@bandd.co>
2013-11-17 04:02:18 -0500

    trying sth out

---
+++ staticfiles/master.js
@@ -0,0 +1,22 @@
+var timer = 0;
+$(window).load(function(){
+	var len = $('.js-load').length;
+	$('.js-load').each(function(index, elements){
+		timer = timer + 50;
+		var $this = $(this);
+		setTimeout(function(){
+			$this.removeClass('hidden').addClass('load-animation')
+			if (index == len -1) { 
+				$('#prompt-countdown').countdown({
+					until: 59, 
+					layout: '{sn} {sl}', 
+					onExpiry: function() {
+						$('#id_body').attr('disabled', 'true');
+						$(this).text('…nevermind');
+						$('#id_author_name').focus();
+					}
+				});
+			}
+		}, timer);
+	});
+});
--- typequoting/settings.py
+++ typequoting/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -96,7 +96,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
